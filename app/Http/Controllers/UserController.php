6d79a2e57f2dacb6f035602282aaad08f33883e5
Jario Neto <jarioneto@gmail.com>
2019-06-04 22:04:09 -0300

    Ajuste

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,11 +8,6 @@ use App\User;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $users = User::orderBy('id', 'asc')->get();
